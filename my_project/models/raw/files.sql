2ca4eff9e4fd0d096148b683b503c53272063fa7
Cynthia Huang <29718979+cynthiahqy@users.noreply.github.com>
2024-09-23 21:56:00 +0000

    [workshop] add model to get file name & metadata

--- my_project/models/raw/files.sql
+++ my_project/models/raw/files.sql
@@ -1,4 +1,10 @@
-select * EXCLUDE(content)
+select * EXCLUDE(content),
+    case
+        when "file" like '%/option_%' then 'stock'
+        when "file" like '%/ticker_history_%' then 'price'
+        when "file" like '%/ticker_info_%' then 'info'
+        else 'unknown'
+    end as entity,
 from glob('s3://us-prd-motherduck-open-datasets/stocks/**/*') as files
 left join read_blob('s3://us-prd-motherduck-open-datasets/stocks/**/*') as meta_data
   on meta_data."filename" = files."file"
